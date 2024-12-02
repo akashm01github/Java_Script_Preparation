a80d83770c2b0bae38cbe4355689c25ffe660d10
akashm01github <akashmukhopadhayay@gmail.com>
2024-12-02 22:15:47 +0530

    Java Script Day-1.

---
+++ Day-1/index.js
@@ -0,0 +1,72 @@
+// console.log("Hello World!");
+
+// console.log("Welcome to Apna College");
+
+// console.log("Akash Mukherjee");
+
+// console.log("I love Java Script");
+
+
+// Variable Declaration in js
+
+// my_name = "Tony Strak";
+
+// console.log(my_name);
+
+// price = 99.95;
+
+// console.log(price)
+
+
+// Let keyword
+
+let fullName = "Akash Mujherjee";
+
+let age =  23;
+
+let totalPrice = 5000;
+
+console.log("My name is: "+fullName);
+
+console.log("My Age is: "+age);
+
+console.log("My Price is: "+totalPrice);
+
+
+const PI = 3.14;
+
+console.log(PI);
+
+
+var m_name  = "Aparna";
+
+console.log(m_name);
+
+
+// Data Types In Java Script
+
+let my_age = 23;
+
+
+const student = {
+    std_name: "Akash Mukherjee",
+    std_age: 23,
+    cgpa: 7.65,
+    isPass:true
+};
+
+
+// For update object
+
+student["std_name"] = "Aparna Mukherjee";
+
+console.log(student["std_name"]);
+
+// Quiz
+const product = {
+    p_name: "Parker Jotter CT Ball Pen(Black)",
+    ratting: 7002,
+    p_price:270,
+    offer: 5
+};
+
