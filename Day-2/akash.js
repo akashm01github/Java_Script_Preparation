b15488f3f539d722558728e72c13a36c4a650d00
akashm01github <akashmukhopadhayay@gmail.com>
2024-12-03 22:34:40 +0530

    Javascript Day-2.

---
+++ Day-2/akash.js
@@ -0,0 +1,77 @@
+console.log("Day-2 of Java Script");
+
+// Arithmetic Operators
+
+let a  = 5;
+
+let b = 2;
+
+let c  = a+b
+
+
+console.log("a = ", a, "& b = ",b);
+console.log("a + b = ",c);
+
+
+console.log("a + b = ",a+b);
+
+console.log("a - b = ",a-b);
+
+
+console.log("a X b = ",a*b);
+
+
+console.log("a / b = ",a/b);
+
+
+console.log("a % b = ",a%b);
+
+
+console.log("a ** b = ",a**b);
+
+
+// Unary Operator
+
+a++;
+
+console.log("After Increment a = ",a);
+
+
+a--;
+console.log("After Decrement a = ",a);
+
+// Pre-Increment + Post Incremet  and Pre-Decrement + post Decrement 
+
+let num1  = 5;
+
+let num2 = 10;
+
+console.log("The Number1 Is: ",num1,"& Number2 is: ",num2)
+
+console.log("num1 = ",num1++);
+console.log("num1 = ",num1);
+
+
+
+console.log("++num1 = ",++num1);
+console.log("num1 = ",num1);
+
+
+console.log("num1-- = ",num1--);
+console.log("num1 = ",num1);
+
+
+
+console.log("--num1 = ",--num1);
+console.log("num1 = ",num1);
+
+
+// a+= operator
+
+num1 += 10;
+console.log("The Number is = ",num1);
+
+
+num1 -= 2;
+console.log("The Number is = ",num1);
+
---
+++ Day-2/akash_op.js
@@ -0,0 +1,48 @@
+console.log("Day-2 Java Script");
+
+// Comparison Operator in JS 
+
+
+let number = 10;
+
+let number2 = "20";
+
+console.log("Number: ",number,"& Number2: ",number2);
+
+console.log("number == number2 is: ",number==number2); //flase 
+
+
+console.log("number != number2 is: ",number!=number2); //true
+
+
+console.log("number === number2 is: ",number === number2); //true
+
+// Conditional Statement in JS
+
+let my_age = 23;
+
+if(my_age > 18){
+    console.log("You can Give Vote");
+}
+else{
+    console.log("You can not Give Vote.");
+}
+
+// alert("JS Day-2");
+
+// let my_prompt = prompt("Hello");
+
+// console.log(my_prompt);
+
+
+
+let num = prompt("Enter the Number: ");
+
+if(num%5 == 0){
+    console.log("The Number is multiple of 5");
+}
+else{
+    console.log("The Number is not multiple of 5");
+}
+
+
